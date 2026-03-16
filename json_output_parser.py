01c821d4e3d579278bbae80a0b06c115f4ae564e
shruti353 <shrutithakkar4667@gmail.com>
2026-03-16 23:29:33 +0530

    json output parser

--- json_output_parser.py
+++ json_output_parser.py
@@ -1,6 +1,7 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
 import os
 
 load_dotenv()
@@ -9,4 +10,20 @@ llm = ChatGroq(
     groq_api_key=os.getenv("GROQ_API_KEY"),
     model_name="llama-3.1-8b-instant"
 )
-# Prompt 1 → detailed report
+
+parser= JsonOutputParser()
+
+# Prompt → get name, age and city of a fictional person
+template1= PromptTemplate(
+    template = "Give me 5 facts about {topic} \n {format_instruction}",
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+    
+)
+
+chain= template1 | llm | parser
+
+result=chain.invoke({'topic': 'Albert Einstein'})
+
+print(result)
+print(type(result))
